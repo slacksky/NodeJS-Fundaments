bd29cd09f8005696e87bb9b7d9bbf86168f06eb2
Jviv <slacksky@gmail.com>
2020-04-10 19:41:08 -0400

    form corrections and adding a new env variable

--- concepts/environment.js
+++ concepts/environment.js
@@ -1,3 +1,5 @@
-let name = process.env.NOMBRE  || 'SN'
+let name = process.env.NAME  || 'Nameless'
 //acc to the env variables || default value to prevent undefined
+let web = process.env.WEB || 'Im webless'
 console.log('Hi, '+name)
+console.log('My website is, ' +web );
