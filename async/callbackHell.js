43a9986fd4c8531f7824fd61c784a70530a9b044
Jviv <slacksky@gmail.com>
2020-04-12 17:26:32 -0400

    solution stating with recursive funtions

--- async/callbackHell.js
+++ async/callbackHell.js
@@ -1,4 +1,5 @@
 let namae='functy'
+let times = 2
 function hello (name, myCallBack){
     console.log('Hi, my name is '+ name)
     setTimeout(function(){
@@ -15,20 +16,41 @@ function talk(talkCB){
 }
 
 function bye(name, anotherCB){
-setTimeout(function(){
-    console.log(`Bye, ${name}`)
-    anotherCB()
-}, 1000)
+    setTimeout(function(){
+        console.log(`Bye, ${name}`)
+        anotherCB()
+    }, 1000)
 }
+
+function conversation(namae, times,YACB){
+    if(times > 0){
+        talk(function(){
+            conversation(namae, times--,YACB)
+        })
+    }else{
+        bye(namae,YACB)
+    }        
+}//using a recursive function to resolve the problem
+
 
 // -- here is the main part of the process
 
 console.log('starting process...')
-hello(namae,function(){
+hello(namae, function(namae){
+    conversation(namae, times, function(){
+        console.log('finalized')
+    })
+})
+/*hello(namae,function(){
+    bye(namae, function(){
+        console.log('finalized')
+    })
+})*/
+/*hello(namae,function(){
     talk(function(){
         bye(namae, function(){
             console.log('stoping process...')
         })
     })
 
-})
+})*/
