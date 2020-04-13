a6379c3e6317951b7b82f951801cd1cddec6fe08
Jviv <slacksky@gmail.com>
2020-04-12 21:32:04 -0400

    async example completed

--- async/asyncAwait.js
+++ async/asyncAwait.js
@@ -3,12 +3,12 @@ async function Hello(name){//remove the callback
         setTimeout(function(){
             console.log('Hi, my neame is: '+name)
             resolve(name)//change the callback for resolve
-            reject('there is an error')//error message 
+            //reject('there is an error')//error message 
         }, 1500)
 
     })//promise is a global class
 }
-function Talk (name){
+async function Talk (name){
     return new Promise((resolve, reject)=>{
         setTimeout(function(){
             console.log('<insert useless talk>')
@@ -17,7 +17,7 @@ function Talk (name){
     })
 }
 
-function Bye(name){
+async function Bye(name){
     return new Promise((resolve, reject)=>{
         setTimeout(function(){
             console.log('Bye, ', name)
@@ -27,6 +27,14 @@ function Bye(name){
 
 }
 
-//-- execution block
 
-await Hello(name)
+async function main(){
+    let name = await Hello('Jorge')
+    await Talk()
+    await Talk()
+    await Talk()
+    await Bye(name)
+    console.log('stoped  the porcess')
+}
+console.log('started  the porcess')
+main()//th main pint of the async await is to turn async request into an structured orderly request
