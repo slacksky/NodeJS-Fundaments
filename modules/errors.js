5d43aa703b7e17c84174bb5ad9ad70b479e092db
Jviv <slacksky@gmail.com>
2020-04-14 15:29:12 -0400

    error handling example  with many function

--- modules/errors.js
+++ modules/errors.js
@@ -1,10 +1,15 @@
+function anotherFunction(){
+    return itBreaks()
+}
+
 function itBreaks(){
     return 3+z
 }
 try{
-    itBreaks()
+    anotherFunction()
 }catch(err){
     console.error('yare yare daze, the funct crashed')
-    console.error(err)//err.message point to the fault
+    console.error(err.message)//err.message point to the fault
+    console.log('error caught')
 }//the usage is to make sure a possible error doesnt crash the app
 
