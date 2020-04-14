8c39ffa0d9034015256d82a655c90f39eb35f9fd
Jviv <slacksky@gmail.com>
2020-04-14 18:28:29 -0400

    native c++ modules example using node-gyp build

---
+++ modules/natives/build/Release/.deps/Release/obj.target/addon/Hi.o.d
@@ -0,0 +1,17 @@
+cmd_Release/obj.target/addon/Hi.o := g++ '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DBUILDING_NODE_EXTENSION' -I/home/sky/.cache/node-gyp/13.12.0/include/node -I/home/sky/.cache/node-gyp/13.12.0/src -I/home/sky/.cache/node-gyp/13.12.0/deps/openssl/config -I/home/sky/.cache/node-gyp/13.12.0/deps/openssl/openssl/include -I/home/sky/.cache/node-gyp/13.12.0/deps/uv/include -I/home/sky/.cache/node-gyp/13.12.0/deps/zlib -I/home/sky/.cache/node-gyp/13.12.0/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/addon/Hi.o.d.raw   -c -o Release/obj.target/addon/Hi.o ../Hi.cc
+Release/obj.target/addon/Hi.o: ../Hi.cc \
+ /home/sky/.cache/node-gyp/13.12.0/include/node/node.h \
+ /home/sky/.cache/node-gyp/13.12.0/include/node/v8.h \
+ /home/sky/.cache/node-gyp/13.12.0/include/node/v8-internal.h \
+ /home/sky/.cache/node-gyp/13.12.0/include/node/v8-version.h \
+ /home/sky/.cache/node-gyp/13.12.0/include/node/v8config.h \
+ /home/sky/.cache/node-gyp/13.12.0/include/node/v8-platform.h \
+ /home/sky/.cache/node-gyp/13.12.0/include/node/node_version.h
+../Hi.cc:
+/home/sky/.cache/node-gyp/13.12.0/include/node/node.h:
+/home/sky/.cache/node-gyp/13.12.0/include/node/v8.h:
+/home/sky/.cache/node-gyp/13.12.0/include/node/v8-internal.h:
+/home/sky/.cache/node-gyp/13.12.0/include/node/v8-version.h:
+/home/sky/.cache/node-gyp/13.12.0/include/node/v8config.h:
+/home/sky/.cache/node-gyp/13.12.0/include/node/v8-platform.h:
+/home/sky/.cache/node-gyp/13.12.0/include/node/node_version.h:
