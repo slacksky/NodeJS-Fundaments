462714e4f6ff7452a34f6dbcc50ddeef8158ebc6
Jviv <slacksky@gmail.com>
2020-04-14 17:46:16 -0400

    spawn example prints the pid and ls

--- modules/childprocess.js
+++ modules/childprocess.js
@@ -1,9 +1,19 @@
-const { exec }=require('child_process')
+const { exec, spawn }=require('child_process')
 
-exec('node modules/console.js', (err,stdout, sterr)=>{
+
+
+
+/*exec('node modules/console.js', (err,stdout, sterr)=>{
     if(err){
         console.error(err)
         return false
     }
     console.log(stdout)
+})*/
+let process =spawn('ls', ['-la'])//command, [parameters]
+
+console.log(process.pid)
+console.log(process.connected)
+process.stdout.on('data', function(datas){
+    console.log(datas.toString())
 })
