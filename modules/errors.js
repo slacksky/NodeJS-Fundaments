c76d5a78dd17b0c406221cad6fd2e5215aedeec1
Jviv <slacksky@gmail.com>
2020-04-14 16:47:55 -0400

    error handling example  with async function

--- modules/errors.js
+++ modules/errors.js
@@ -5,8 +5,25 @@ function anotherFunction(){
 function itBreaks(){
     return 3+z
 }
+function itBreaksAsync(cb){
+    setTimeout(function(){
+        try{
+            return 3+z
+        }catch(err){
+            console.error('Error in the async funct')//erro catching in asyn has to happen in the functions
+            cb(err)
+        }        
+    },1000)
+}
+
+
+/*execution block */
+
 try{
-    anotherFunction()
+    //anotherFunction()
+    itBreaksAsync(function(err){
+        console.log(err.message)
+    })//its not caught because it is in the event loop, not in the main loop
 }catch(err){
     console.error('yare yare daze, the funct crashed')
     console.error(err.message)//err.message point to the fault
