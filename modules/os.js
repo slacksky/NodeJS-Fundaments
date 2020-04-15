a05d42d4a28dc9218e982e37c272008b4d83b5dd
Jviv <slacksky@gmail.com>
2020-04-15 17:30:59 -0400

    os example os fremem  bytes kb mb and gb

--- modules/os.js
+++ modules/os.js
@@ -4,4 +4,15 @@ const os = require('os')
 //console.log(os.platform())
 //console.log(os.cpus())
 //console.log(os.cpus().length)
-console.log(os.constants)
+//console.log(os.constants)
+
+const SIZE = 1024
+
+kb =(bytes) => bytes / SIZE //arrow funct kb
+mb =(bytes) => kb(bytes) / SIZE //arrow funct mb
+gb =(bytes) => mb(bytes) / SIZE //arrow funct gb
+
+console.log(`${os.freemem()} bytes`)
+console.log(`${kb(os.freemem())} kbtyes`)
+console.log(`${mb(os.freemem())} mbtyes`)
+console.log(`${gb(os.freemem())} gbtyes`)
