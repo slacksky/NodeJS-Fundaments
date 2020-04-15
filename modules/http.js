6030a36112b3490933486ea4993091d9be8881f1
Jviv <slacksky@gmail.com>
2020-04-15 16:09:49 -0400

    http example, request events

--- modules/http.js
+++ modules/http.js
@@ -1,11 +1,26 @@
 const http = require('http')//import http module
-http.createServer(function(req, res){//request  y response
-    console.log('new Request')
-    console.log(req.url)
-    res.writeHead(201, {'Content-Type': 'test/plain'})
-    res.write('Hi, know I know how to use http in Nodejs')
 
-    res.end()
-}).listen(3000)
+http.createServer(router).listen(3000)
 
 console.log('listening on port 3000')
+
+
+function router(req, res){//request  y response
+    console.log('new Request')
+    console.log(req.url)
+    
+    switch(req.url){
+        case '/hello':
+            res.write('howdy')
+            res.end
+            break
+        default:
+            res.write('error 404 :cant hear your request')
+            res.end    
+    }
+
+    //res.writeHead(201, {'Content-Type': 'test/plain'})
+    //res.write('Hi, know I know how to use http in Nodejs')
+
+    //res.end()
+}
