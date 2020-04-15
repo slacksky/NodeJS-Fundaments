7355ee77a09783fbd508f05a02ce7ae7d0eb6354
Jviv <slacksky@gmail.com>
2020-04-15 17:42:08 -0400

    os example os home dir print print out

--- modules/os.js
+++ modules/os.js
@@ -6,7 +6,7 @@ const os = require('os')
 //console.log(os.cpus().length)
 //console.log(os.constants)
 
-const SIZE = 1024
+/*const SIZE = 1024
 
 kb =(bytes) => bytes / SIZE //arrow funct kb
 mb =(bytes) => kb(bytes) / SIZE //arrow funct mb
@@ -15,4 +15,6 @@ gb =(bytes) => mb(bytes) / SIZE //arrow funct gb
 console.log(`${os.freemem()} bytes`)
 console.log(`${kb(os.freemem())} kbtyes`)
 console.log(`${mb(os.freemem())} mbtyes`)
-console.log(`${gb(os.freemem())} gbtyes`)
+console.log(`${gb(os.freemem())} gbtyes free of ${gb(os.totalmem())} toal mem`)
+*/
+console.log(os.homedir())
