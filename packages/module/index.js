db35350a2e1cdf499ffa4fff9a43e9550ba233c2
Jviv <slacksky@gmail.com>
2020-04-15 21:00:45 -0400

    moduel example basic example

--- packages/module/index.js
+++ packages/module/index.js
@@ -1,5 +1,8 @@
 //bring in the module 
-
+const modulo= require('./module.js')//use normal route to get the local module since it's installed 
 
 
 //and execute a function
+
+//console.log(modulo)
+modulo()
--- packages/module/module.js
+++ packages/module/module.js
@@ -1,1 +1,3 @@
-SayHi=()=>console.log('Hi, world')
+sayHi=()=>console.log('Hi, world')
+
+module.exports = sayHi// without exporting no function will be passed on to the index.js
