722e1365053062b24efa7b9f8ee9011021ff2b4d
Jviv <slacksky@gmail.com>
2020-04-16 21:50:58 -0400

    bcrypt hash example and comparizon for confirmation

--- packages/tools/bcrypt.js
+++ packages/tools/bcrypt.js
@@ -4,4 +4,10 @@ const password = '1234Share?'
 
 bcrypt.hash(password, 5, function(err, hash){
     console.log(hash)
-})//pass, rounds
+    bcrypt.compare(password, hash,function(err, res){
+        if(err){
+            console.log(err)
+        }
+        console.log(res)
+    } )
+})
