ba7c49dfd7dbeabaa5f20d5ea0c09c7fa4555ca7
Jviv <slacksky@gmail.com>
2020-04-17 21:22:49 -0400

    buffer  example ABC

--- memory/buffer.js
+++ memory/buffer.js
@@ -1,5 +1,18 @@
 //let buffer = Buffer.alloc(4)
 //let buffer = Buffer.from([1,2,3,4])
-let buffer = Buffer.from('Hello')
+//let buffer = Buffer.from('Hello')
 
-console.log(buffer)
+//console.log(buffer)
+
+
+//--execution block
+
+let abc = Buffer.alloc(26)
+console.log(abc)
+
+for(let i=0;i<26;i++){
+    abc[i]=i + 97 //number corresponds to the asci offset to the a
+}
+
+console.log(abc)
+console.log(abc.toString())
