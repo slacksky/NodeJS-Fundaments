0737bd9316334228e17ee668f7c79d7c20bac5ba
Jviv <slacksky@gmail.com>
2020-04-19 20:17:38 -0400

    example of stream read, big files example

--- memory/stream.js
+++ memory/stream.js
@@ -3,6 +3,11 @@ let data = ''
 let readableStream =fs.createReadStream(__dirname+'/input.txt')
 
 readableStream.setEncoding('UTF8')
+
 readableStream.on('data',function(chunk){
-    console.log(chunk)
+    data+=chunk
 })
+
+readableStream.on('end',function(){
+    console.log(data)
+})//example for big stream
