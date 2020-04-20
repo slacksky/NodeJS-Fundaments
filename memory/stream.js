a03f222b2ba1a425264cfd260260573cb63127fb
Jviv <slacksky@gmail.com>
2020-04-19 20:12:38 -0400

    example of stream read, setting encoding option added as default

--- memory/stream.js
+++ memory/stream.js
@@ -2,6 +2,7 @@ const fs =require('fs')
 let data = ''
 let readableStream =fs.createReadStream(__dirname+'/input.txt')
 
+readableStream.setEncoding('UTF8')
 readableStream.on('data',function(chunk){
-    console.log(chunk.toString())
+    console.log(chunk)
 })
