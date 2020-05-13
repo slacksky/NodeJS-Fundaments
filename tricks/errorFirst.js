0ee570ad1d8e21bffcd55f69550d29ea81b26e2e
Jviv <slacksky@gmail.com>
2020-05-12 22:55:57 -0400

    error first example  how  not to catch an asinc error

--- tricks/errorFirst.js
+++ tricks/errorFirst.js
@@ -9,12 +9,18 @@ function asincronic(callback){
     },1000)
 }
 
-asincronic(function(err, dat){
-    if(err){
-        //console.error('houston we have a problem')
-        //console.error(err)
-        throw err //alt way to stop execution
-        //return false
-    }
-    console.log('all good in the console front, this is my data', dat )
-})
+
+try {
+    asincronic(function(err, dat){
+        if(err){
+            //console.error('houston we have a problem')
+            //console.error(err)
+            throw err //alt way to stop execution
+            //return false
+        }
+        console.log('all good in the console front, this is my data', dat )
+    })    
+} catch (e) {
+    console.error('error caught')
+    console.error(e)
+}//example not caught, because async function  
